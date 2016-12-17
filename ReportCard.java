65db40241fae8bc9a9c0dc9138bb795b45088731
dwhathaway1 <dwhathaway@gmail.com>
2016-12-17 10:34:51 -0500

    Create ReportCard.java

---
+++ ReportCard.java
@@ -0,0 +1,31 @@
+public class ReportCard{
+
+private String mSubjectTaken;
+
+private String mGradeGiven;
+
+public ReportCard(String subjectTaken, String gradeGiven){
+
+mSubjectTaken = subjectTaken;
+
+mGradegiven = gradeGiven;
+}
+
+/**
+*Get the subject of a course.
+*/
+public String getSubjectTaken(){
+return mSubjectTaken;
+}
+
+/**
+*Get the grade given to a student for a subject.
+*/
+
+public String getGradeGiven(){
+return mGradeGiven;
+}
+
+ReportCard r1 = new ReportCard (
+
+}
